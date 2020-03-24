3d6dcc592c333200b4ecaa2e8068174e76733b7c
BrenoSantosAlmeida <breno.almeida@sagatechbrasil.com.br>
2020-03-24 09:54:17 -0300

    Finalizando jogo da velha.

--- Jogo_Da_Velha/app/src/main/java/com/example/jogo_da_velha/Activities/MainActivity.java
+++ Jogo_Da_Velha/app/src/main/java/com/example/jogo_da_velha/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jogo_da_velha.Activities;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,12 +17,9 @@ public class MainActivity extends AppCompatActivity {
     private Button[] btn;
     private Button btnReset;
     private TextView result;
-    private static int player;
-    public static int imgCircle = R.drawable.imgcircle;
-    public static int imgx = R.drawable.ximg;
+    private char player;
     private int count = 1;
     private int[][] game = {{0,0,0},{0,0,0},{0,0,0}};
-    private int[] WhiteList = {1,2,3,4,5,6,7,8,9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +35,23 @@ public class MainActivity extends AppCompatActivity {
         btn[6] = findViewById(R.id.button7);
         btn[7] = findViewById(R.id.button8);
         btn[8] = findViewById(R.id.button9);
-
+        result = findViewById(R.id.textView);
+        btnReset = findViewById(R.id.buttonReset);
 
 
         for(int i = 0; i<9; i++)
         {
-            btn[i].setOnClickListener(new click);
+            btn[i].setOnClickListener(CLICKING);
         }
     }
 
-    private View.OnClickListener click = new View.OnClickListener (){
-        public void Onclick (View v)
+    private View.OnClickListener CLICKING = new View.OnClickListener ()
+    {
+        @Override
+        public void onClick (View v)
         {
             LogicGame logicGame = new LogicGame();
-            logicGame.getImage(count, imgx, imgCircle, player);
+            getImage(count);
             if (logicGame.verifyWinner(game, count) == true)
             {
                 if (count % 2 == 0)
@@ -66,61 +67,62 @@ public class MainActivity extends AppCompatActivity {
             switch (v.getId())
             {
                 case R.id.button1 :
-                    if (verifyContent(1, WhiteList)) {
                         btn[0].setText(player);
-                        btn[0].setEnable(false);
-                    }
+                        btn[0].setEnabled(false);
+                        game[0][0] = 1;
                     break;
                 case R.id.button2 :
-                    if (verifyContent(2 ,WhiteList)) {
                         btn[1].setText(player);
-                        btn[1].setEnable(false);
-                    }
+                        btn[1].setEnabled(false);
+                        game[0][1] = 1;
                     break;
                 case R.id.button3 :
-                    if (verifyContent(3, WhiteList)){
                         btn[2].setText(player);
-                        btn[2].setEnable(false);
-                    }
-
+                        btn[2].setEnabled(false);
+                        game[0][2] = 1;
                     break;
                 case R.id.button4 :
-                    if (verifyContent(4, WhiteList)) {
                         btn[3].setText(player);
-                        btn[3].setEnable(false);
-                    }
+                        btn[3].setEnabled(false);
+                        game[1][0] = 1;
                     break;
                 case R.id.button5 :
-                    if (verifyContent(5, WhiteList)) {
                         btn[4].setText(player);
-                        btn[4].setEnable(false);
-                    }
+                        btn[4].setEnabled(false);
+                        game[1][1] = 1;
                     break;
                 case R.id.button6 :
-                    if(verifyContent(6, WhiteList)) {
                         btn[5].setText(player);
-                        btn[5].setEnable(false);
-                    }
+                        btn[5].setEnabled(false);
+                        game[1][2] = 1;
                     break;
                 case R.id.button7 :
-                    if (verifyContent(7, WhiteList)) {
                         btn[6].setText(player);
-                        btn[6].setEnable(false);
-                    }
+                        btn[6].setEnabled(false);
+                        game[2][0] = 1;
                     break;
                 case R.id.button8 :
-                    if (verifyContent(8, WhiteList)){
                         btn[7].setText(player);
-                        btn[7].setEnable(false);
-                    }
+                        btn[7].setEnabled(false);
+                        game[2][1] = 1;
                     break;
                 case R.id.button9 :
-                    if (verifyContent(9, WhiteList)) {
                         btn[8].setText(player);
-                        btn[8].setEnable(false);
-                    }
+                        btn[8].setEnabled(false);
+                        game[2][2] = 1;
                     break;
             }
+        }
+    };
+
+    public void getImage(int count)
+    {
+        if (count%2 == 0)
+        {
+            this.player = 'O';
+        }
+        else {
+            this.player = 'X';
         }
     }
 }
--- Jogo_Da_Velha/app/src/main/java/com/example/jogo_da_velha/Models/LogicGame.java
+++ Jogo_Da_Velha/app/src/main/java/com/example/jogo_da_velha/Models/LogicGame.java
@@ -6,52 +6,33 @@ import android.view.View;
 import com.example.jogo_da_velha.R;
 
 public class LogicGame {
-
-    public void getImage(int count, int x, int circle, int player)
-    {
-        if (count%2 == 0)
-        {
-            player = x;
-        }
-        else {
-            player = circle;
-        }
-    }
-
-    public boolean verifyContent(int btn, int[] WhiteList)
-    {
-        for (int i = 0; i <= WhiteList.length; i++)
-        {
-            if (WhiteList[i] == btn)
-            {
-                WhiteList[i] = 0;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean verifyWinner(int[][] matriz, int count)
+    public boolean verifyWinner(int[][] game, int count)
     {
         if (count < 9)
         {
             for(int i=0; i<3; i++)
             {
-                if(matriz[0][i] == 1 && matriz[1][i] == 1 && matriz[2][i] == 1)
+                if(game[0][i] == 1 && game[1][i] == 1 && game[2][i] == 1)
                 {
                     return true;
-                }else if(matriz[i][0] == 1 && matriz[i][1] == 1 && matriz[i][2] == 1)
+                }
+                else if(game[i][0] == 1 && game[i][1] == 1 && game[i][2] == 1)
                 {
                     return true;
                 }
             }
 
-            if(matriz[0][0] == 1 && matriz[1][1] == 1 && matriz[2][2] == 1)
+            if(game[0][0] == 1 && game[1][1] == 1 && game[2][2] == 1)
             {
                 return true;
-            }else if(matriz[0][2] == 1 && matriz[1][1] == 1 && matriz[2][0] == 1)
+            }
+            else if(game[0][2] == 1 && game[1][1] == 1 && game[2][0] == 1)
             {
                 return true;
+            }
+            else
+            {
+                return false;
             }
         }
         return false;
